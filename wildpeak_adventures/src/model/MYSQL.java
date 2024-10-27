9cad08b55042cb5eb5a64c076766b905ceec990f
Dushmantha2004 <sandundushmantha83@gmail.com>
2024-10-27 10:41:56 +0530

    update mysql connection

--- wildpeak_adventures/src/model/MYSQL.java
+++ wildpeak_adventures/src/model/MYSQL.java
@@ -14,4 +14,24 @@ import java.sql.ResultSet;
  */
 public class MYSQL {
     
+
+    private static Connection connection;
+
+    public static void createConnection() throws Exception {
+        if (connection == null) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://wiledpeak-adventures.cfsiiq2mmc40.eu-north-1.rds.amazonaws.com:3306/wildpeak", "admin", "%qYMqW*{520{");
+        }
+    }
+
+    public static ResultSet executeSearch(String query) throws Exception {
+        createConnection();
+        return connection.createStatement().executeQuery(query);
+    }
+
+    public static Integer executeIUD(String query) throws Exception {
+        createConnection();
+        return connection.createStatement().executeUpdate(query);
+    }
+
 }
