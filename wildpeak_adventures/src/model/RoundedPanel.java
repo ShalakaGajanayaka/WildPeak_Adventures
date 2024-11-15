dc9c88ba26c6e6ccebb20b5df5d3357eb58b8fc2
ShalakaGajanayaka <shalakagajanayake123@gmail.com>
2024-11-15 14:11:44 +0530

    still update........

---
+++ wildpeak_adventures/src/model/RoundedPanel.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author shalaka
+ */
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.RoundRectangle2D;
+import javax.swing.JPanel;
+
+public class RoundedPanel extends JPanel {
+    private int cornerRadius = 2000;
+    
+    public RoundedPanel() {
+        setOpaque(false);
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g.create();
+        
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        g2.setColor(getBackground());
+        g2.fill(new RoundRectangle2D.Double(0, 0, getWidth()-1, getHeight()-1, cornerRadius, cornerRadius));
+        
+        g2.dispose();
+    }
+}
