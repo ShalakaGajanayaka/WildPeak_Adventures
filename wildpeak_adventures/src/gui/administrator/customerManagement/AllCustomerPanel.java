8c714239bab13aba0a083b5bd0f6552eb7d53248
ShalakaGajanayaka <shalakagajanayake123@gmail.com>
2024-12-17 18:39:49 +0530

    Reapply "Merge branch 'customer-update-3'"
    This reverts commit f572a0fcd558eb6fc1efa40bea0087dabd123278.

--- wildpeak_adventures/src/gui/administrator/customerManagement/AllCustomerPanel.java
+++ wildpeak_adventures/src/gui/administrator/customerManagement/AllCustomerPanel.java
@@ -8,6 +8,7 @@ package gui.administrator.customerManagement;
 import java.util.Vector;
 import javax.swing.table.DefaultTableModel;
 import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import model.MYSQL;
 
 /**
@@ -16,14 +17,17 @@ import model.MYSQL;
  */
 public class AllCustomerPanel extends javax.swing.JPanel {
 
+  
+
     /**
      * Creates new form AllCustomerPanel
      */
     public AllCustomerPanel() {
+        
         initComponents();
 
-        filter.add(new Filter());
-        customercount.add(new CustomersCount());
+        filter.add(new Filter(this));
+        customercount.add(new CustomersCount(this));
 
         loadCustomer();
     }
@@ -42,7 +46,7 @@ public class AllCustomerPanel extends javax.swing.JPanel {
                 String fname = resultSet.getString("customer.fname");
                 String lname = resultSet.getString("customer.lname");
                 Vector<String> vector = new Vector<>();
-                vector.add(fname +" "+ lname);
+                vector.add(fname + " " + lname);
 //                vector.add(resultSet.getString("customer.lname"));
                 vector.add(resultSet.getString("customer.email"));
                 vector.add(resultSet.getString("customer.mobile"));
@@ -71,6 +75,45 @@ public class AllCustomerPanel extends javax.swing.JPanel {
             e.printStackTrace();
         }
     }
+    
+     public void loadCustomer(String column, String orderby, String searchText) {
+        try {
+            String query = "SELECT * FROM `customer` "
+                    + "INNER JOIN `gender` ON `customer`.`gender_id` = `gender`.`id` "
+                    + "INNER JOIN `customer_type` ON `customer`.`customer_type_id` = `customer_type`.`id` "
+                    + "WHERE `customer`.`fname` LIKE ? OR `customer`.`lname` LIKE ? OR `customer`.`email` LIKE ? OR `customer`.`mobile` LIKE ?";
+
+            PreparedStatement preparedStatement = MYSQL.getConnection().prepareStatement(query);
+            preparedStatement.setString(1, "%" + searchText + "%"); // fname
+            preparedStatement.setString(2, "%" + searchText + "%"); // lname
+            preparedStatement.setString(3, "%" + searchText + "%"); // email
+            preparedStatement.setString(4, "%" + searchText + "%"); // mobile
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            DefaultTableModel defaultTableModel = (DefaultTableModel) jTable1.getModel();
+            defaultTableModel.setRowCount(0);
+
+            while (resultSet.next()) {
+                String fname = resultSet.getString("customer.fname");
+                String lname = resultSet.getString("customer.lname");
+                Vector<String> vector = new Vector<>();
+                vector.add(fname + " " + lname);
+                vector.add(resultSet.getString("customer.lname"));
+                vector.add(resultSet.getString("customer.email"));
+                vector.add(resultSet.getString("customer.mobile"));
+                vector.add(resultSet.getString("customer.age"));
+                vector.add(resultSet.getString("customer.register_date"));
+                vector.add(resultSet.getString("gender.name"));
+                vector.add(resultSet.getString("customer_type.name"));
+
+                defaultTableModel.addRow(vector);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -94,7 +137,13 @@ public class AllCustomerPanel extends javax.swing.JPanel {
         jLabel1.setText("Search");
 
         jTextField1.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
+        jTextField1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jTextField1KeyReleased(evt);
+            }
+        });
 
+        filter.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         filter.setLayout(new java.awt.CardLayout());
 
         jButton1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
@@ -124,6 +173,7 @@ public class AllCustomerPanel extends javax.swing.JPanel {
         });
         jScrollPane1.setViewportView(jTable1);
 
+        customercount.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         customercount.setLayout(new java.awt.CardLayout());
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -157,16 +207,21 @@ public class AllCustomerPanel extends javax.swing.JPanel {
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton2)
+                .addComponent(jButton2)
+                .addGap(10, 10, 10)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(customercount, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(filter, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(customercount, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 137, Short.MAX_VALUE)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField1KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField1KeyReleased
+         String searchtext = jTextField1.getText();
+        loadCustomer("id", "ASC", searchtext);
+    }//GEN-LAST:event_jTextField1KeyReleased
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -176,7 +231,7 @@ public class AllCustomerPanel extends javax.swing.JPanel {
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    public javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
