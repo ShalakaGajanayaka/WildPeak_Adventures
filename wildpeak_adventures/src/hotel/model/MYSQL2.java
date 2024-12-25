fcdac5c7a67576b01e75452db1dffd0b3c6ed1e6
ShalakaGajanayaka <shalakagajanayake123@gmail.com>
2024-12-25 14:28:28 +0530

    updated

---
+++ wildpeak_adventures/src/hotel/model/MYSQL2.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotel.model;
+
+import model.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sandu
+ */
+public class MYSQL2 {
+
+    private static Connection connection;
+
+    public static void createConnection() throws Exception {
+        if (connection == null) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://grandhaven-hotelmanagement.cfsiiq2mmc40.eu-north-1.rds.amazonaws.com:3306/wildpeak", "admin", "%qYMqW*{520{");
+        }
+    }
+
+    // Get the connection object
+    public static Connection getConnection() throws SQLException, Exception {
+        createConnection();  // Ensure the connection is created
+        return connection;    // Return the connection
+    }
+
+    public static ResultSet executeSearch(String query) throws Exception {
+        createConnection();
+        return connection.createStatement().executeQuery(query);
+    }
+
+    public static Integer executeIUD(String query) throws Exception {
+        createConnection();
+        return connection.createStatement().executeUpdate(query);
+    }
+
+}
